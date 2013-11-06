667b1965a1ae496aa54cee9d2969a682ce94eeb0
Dave Makena <davemakena@gmail.com>
2013-11-05 19:05:46 -0800

    added readme

--- spec/hawkeye_spec.rb
+++ spec/hawkeye_spec.rb
@@ -4,44 +4,25 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
   require 'pry'
   require 'nokogiri'
 
-describe "Hawkeye" do
+describe "Hawkeye usage" do
 
-  it "refreshes" do
-    puts
-    puts "# == Hawkeye.refresh(memory, url, css_selector, item_tag, top_id)"
-    puts "memory = #{memory=''}"
-    puts "url = #{url="http://test.html"}"
-    puts "css_selector = #{css_selector='a'}"
-    puts "item_tag = #{item_tag='ul'}"
-    puts "top_id = #{top_id='top'}"
-    change=Hawkeye.refresh(memory, url, css_selector, item_tag, top_id)
-    puts "change = Hawkeye.refresh(memory, url, css_selector, item_tag, top_id) #=> #{change}"
-    puts
-    puts "Hawkeye.show(change) #=> #{change}"
-    change=Hawkeye.refresh(change, url, css_selector, item_tag, top_id)
-    puts "change = Hawkeye.refresh(change, url, css_selector, item_tag, top_id) #=> #{change}"
-    puts
-    puts "Hawkeye.show(change) #=> #{change}"
-  end
-
-  it "shows" do
+  it "refresh and show change in content" do
     puts
     puts "# == Hawkeye.show(memory)"
-    puts "memory = #{memory=''}"
+    puts "memory = \"#{memory=''}\""
     puts "url = #{url="http://test.html"}"
     puts "css_selector = #{css_selector='a'}"
     puts "item_tag = #{item_tag='ul'}"
     puts "top_id = #{top_id='top'}"
-    puts
-    puts "Hawkeye.show(memory) #=> #{memory}"
-    memory=Hawkeye.refresh(memory, url, css_selector, item_tag, top_id)
-    puts "memory = Hawkeye.refresh(memory, url, css_selector, item_tag, top_id) #=> #{memory}"
-    puts
-    puts "Hawkeye.show(memory) #=> #{memory}"
-    memory=Hawkeye.refresh(memory, url, css_selector, item_tag, top_id)
-    puts "memory = Hawkeye.refresh(memory, url, css_selector, item_tag, top_id) #=> #{memory}"
-    puts
-    puts "Hawkeye.show(memory) #=> #{memory}"
+    puts "memory = Hawkeye.refresh(memory, url, css_selector, item_tag, top_id)"
+    puts "memory #=> #{memory=Hawkeye.refresh(memory, url, css_selector, item_tag, top_id)}"
+    puts "Hawkeye.show(memory) #=> \"#{Hawkeye.show(memory)}\""
+    puts "memory = Hawkeye.refresh(memory, url, css_selector, item_tag, top_id)"
+    puts "memory #=> #{memory=Hawkeye.refresh(memory, url, css_selector, item_tag, top_id)}"
+    puts "Hawkeye.show(memory) #=> \"#{Hawkeye.show(memory)}\""
+    puts "memory = Hawkeye.refresh(memory, url, css_selector, item_tag, top_id)"
+    puts "memory #=> #{memory=Hawkeye.refresh(memory, url, css_selector, item_tag, top_id)}"
+    puts "Hawkeye.show(memory) #=> \"#{Hawkeye.show(memory)}\""
   end
 
 end
